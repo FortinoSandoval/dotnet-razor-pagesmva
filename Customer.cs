66cd2c9628bc07b3768921baab4a8cbdd4a7910e
Fortino Sandoval <Fortino.Sandoval@maritz.com>
2018-11-30 22:15:16 -0800

    initial commit

---
+++ Customer.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace razorPagesApp
+{
+  public class Customer
+  {
+    public int Id { get; set; }
+    [Required, StringLength(10)]
+    public string Name { get; set; }
+  }
+}
